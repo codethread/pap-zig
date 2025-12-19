f18b1853d4988f3d67ea67841d0bd02cf9598b71
codethread <adamhalldesigns@gmail.com>
2025-12-19 04:18:09 +0000

    more code

--- src/day01/solution.zig
+++ src/day01/solution.zig
@@ -22,37 +22,56 @@ const WORD_REG_FIELD = [_][]const u8{
     "di", // 111
 };
 
+inline fn wBit(byte: u8) bool {
+    return (byte & 0b1) == 1;
+}
+inline fn modField(byte: u8) u8 {
+    return byte >> 6;
+}
+inline fn rmField(byte: u8) u8 {
+    return byte & 0b111;
+}
+inline fn regField2(byte: u8) u8 {
+    return byte >> 3 & 0b111;
+}
+
 pub fn solve(writer: *std.Io.Writer, input: []const u8) !void {
-    const instruction_len = input.len;
     var i: u32 = 0;
 
-    while (i < instruction_len) {
+    while (i < input.len) {
         const byte = input[i];
-        const opcode = (byte >> 2);
 
-        switch (opcode) {
+        switch (byte) {
             // mov
             // register to/from register
-            0b100010 => {
+            0b10001000...0b10001011 => {
                 // const d = byte >> 1 & 0b1 == 1;
-                const w = (byte & 0b1) == 1;
+                const w = wBit(byte);
 
                 // get next byte
                 i += 1;
                 const byte_2 = input[i];
-                const mod_byte = byte_2 >> 6;
-                const reg_byte = byte_2 >> 3 & 0b111;
-                const rm_byte = byte_2 & 0b111;
+                const mod_byte = modField(byte_2);
+                const reg_byte = regField2(byte_2);
+                const rm_byte = rmField(byte_2);
 
                 if (mod_byte != 0b11) {
-                    unreachable;
+                    @panic("noop");
                 }
-
                 const reg_from = if (w) WORD_REG_FIELD[reg_byte] else BYTE_REG_FIELD[reg_byte];
                 const reg_to = if (w) WORD_REG_FIELD[rm_byte] else BYTE_REG_FIELD[rm_byte];
+
                 try writer.print("mov {s}, {s}\n", .{ reg_to, reg_from });
             },
-            else => unreachable,
+            // immidiate to register/memory
+            0b1100001, 0b11000111 => {
+                @panic("noop");
+            },
+            // immidiate to register
+            0b1011000, 0b1011111 => {
+                @panic("noop");
+            },
+            else => @panic("noop"),
         }
 
         i += 1;
